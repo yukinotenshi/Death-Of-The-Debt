7f949310aae1bfd29f1322a7059c35fd6b1c19ce
ahmadfahmip <ahmad.fahmi.pratama@gmail.com>
2019-03-05 16:09:27 +0700

    Add state and mutations on Room

--- src/store/index.js
+++ src/store/index.js
@@ -12,6 +12,12 @@ export default new Vuex.Store({
       id: '',
       username: '',
     },
+    room: {
+      chasing_team: [],
+      hiding_team: [],
+      is_owner: '',
+      room_id: '',
+    },
     baseUrl: config.baseUrl,
   },
   plugins: [createPersistedState()],
@@ -25,6 +31,16 @@ export default new Vuex.Store({
       state.user.token = data.access_token;
       state.user.id = data.id;
       state.user.username = data.username;
+    },
+    setRoom(state, data) {
+      state.room.room_id = data.room_id;
+      state.room.is_owner = data.is_owner;
+    },
+    addChasingTeam(state, username) {
+      state.room.chasing_team.push(username);
+    },
+    addHidingTeam(state, username) {
+      state.room.hiding_team.push(username);
     }
   }
 })
