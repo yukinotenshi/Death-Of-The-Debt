9faba667c873d72ccdb7f2c99e052693e01e752f
dichi <13516063@std.stei.itb.ac.id>
2019-03-05 12:20:07 +0700

    added room code to PlayOptionsContents

--- src/components/partials/mainmenu/PlayOptionsContents.vue
+++ src/components/partials/mainmenu/PlayOptionsContents.vue
@@ -1,10 +1,19 @@
 <template>
   <v-container fluid>
-    <game-button
-      v-on:action="gotoCreateRoom"
-      title="Create Room"
-    />
-    <game-button title="Join Room" />
+    <div class="createroom">
+      <game-button
+        v-on:action="gotoCreateRoom"
+        title="Create Room"
+      />
+      <div class="gap">
+        <h3>Room Code</h3>
+        <input type="text" v-model="roomCode">
+      </div>
+      <game-button 
+        v-on:action="joinRoom"
+        title="Join Room"
+      />
+    </div>
   </v-container>
 </template>
 
@@ -17,14 +26,47 @@ export default {
   components: {
     GameButton,
   },
+  data() {
+    return {
+      roomCode : ''
+    }
+  },
   methods: {
     gotoCreateRoom() {
       this.$router.push({ name: 'room' });
+    },
+    joinRoom() {
+      this.roomCode = "ahuahuahuahhauahuahuahuahuahauhauha";
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.createroom {
+  text-align: center;
+  width: 100%;
+
+  .gap {
+    margin-top: 1rem;
+    display: inline-block;
+
+    h3 {
+      display: inline;
+      margin: 0 1rem;
+    }
+  }
+
+  input[type=text] {
+    font-size: 1.25rem;
+    border: 2px solid gray;
+    border-radius: 20px;
+    background-color: white;
+    padding: 0.5rem;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;    
+    box-sizing: border-box;
+  }  
+}
 
 </style>
