1321577c5137912d2a7e690a887366374ec3b802
ahmadfahmip <ahmad.fahmi.pratama@gmail.com>
2019-03-20 14:34:59 +0700

    Design main menu

---
+++ src/components/partials/gameplay/GameCurrItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <img :src="source" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GameCurrItem',
+  props: {
+    name: String,
+  },
+  data() {
+    return {
+      source: '@assets/img/items/',
+    };
+  },
+  created() {
+    // alert(this.source);
+    this.source += `${this.name}.png`;
+    this.source = require(this.source);
+    // alert(this.source);
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/components/screens/MainMenu.vue
+++ src/components/screens/MainMenu.vue
@@ -7,9 +7,10 @@
     >
       <component :is="childComponent" />
     </closable-board>
-    <div id="menu__logo">
+    <div id="menu__chara">
       <img
-        src="./../../assets/img/logo.png"
+        id="splash__chara"
+        src="./../../assets/img/splash/chara.png"
         alt="logo"
       >
     </div>
@@ -26,6 +27,12 @@
       >
         <game-button title="Options" />
       </router-link>
+    </div>
+    <div id="menu__logo">
+      <img
+        src="./../../assets/img/logo.png"
+        alt="logo"
+      >
     </div>
   </v-container>
 </template>
@@ -57,19 +64,37 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$yellow: #D2B24F;
+
 #menu {
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   height: 100vh;
+
+  background: url('./../../assets/img/backgrounds/home.jpg');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+
+#menu__chara, #menu__logo, #menu__buttons {
+  position: absolute;
+}
+
+#menu__chara {
+  img {
+    width: 80vw;
+    margin-bottom: 35vh;
+  }
 }
 
 #menu__logo {
   img {
-    width: 70vw;
+    width: 60vw;
     height: auto;
-    margin-bottom: 5vh;
+    margin-bottom: 20vh;
   }
 }
 
@@ -78,10 +103,22 @@ export default {
   flex-direction: column;
   justify-content: center;
   text-align: center;
+  background-color: #6E563C;
+  margin-top: 13vh;
+  padding: 8vh 8vh 5vh 8vh;
+  border-radius: 5vh;
+  border: 0.3rem solid $yellow;
 
   .menu__btn {
-    // margin: 1rem;
-    text-decoration: none;
+    button {
+      text-decoration: none;
+      background-color: $yellow;
+      width: 20vh;
+      color: white;
+      -webkit-box-shadow: 0px 6px 20px 0px rgba(0,0,0,0.17);
+      -moz-box-shadow: 0px 6px 20px 0px rgba(0,0,0,0.17);
+      box-shadow: 0px 6px 20px 0px rgba(0,0,0,0.17);
+    }
   }
 }
 </style>
